5a2eb46e6bdef182ca62f58db80d29d36842ee3a
Adam Turner <act@adamturner.dev>
2024-04-09 11:12:15 -0700

    Bump WP tested-to to 6.5.0 to close #27

--- hrswp-github-updater.php
+++ hrswp-github-updater.php
@@ -9,7 +9,7 @@
  * Update URI: https://api.github.com/repos/washingtonstateuniversity/hrswp-github-updater/releases/latest
  * Text Domain: hrswp-github-updater
  * Requires at least: 5.8
- * Tested up to: 6.4.2
+ * Tested up to: 6.5.0
  * Requires PHP: 7.3
  *
  * @package HRSWP_GitHub_Updater
